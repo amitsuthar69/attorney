8381c4e4ef108769e51fd6fccb711e5258e21512
Amit Suthar <sutharamit707@gmail.com>
2024-12-11 21:50:54 +0530

    fallback port

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module attorney
 
 go 1.23.4
+
+require github.com/joho/godotenv v1.5.1
--- main.go
+++ main.go
@@ -2,14 +2,21 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"attorney/internal/proxy"
+
+	_ "github.com/joho/godotenv/autoload"
 )
 
 func main() {
 	// Proxy with default config
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
 	p := proxy.NewProxy(proxy.Config{
-		ListenAddr: ":8888",
+		ListenAddr: ":" + port,
 	})
 
 	if err := p.Start(); err != nil {
